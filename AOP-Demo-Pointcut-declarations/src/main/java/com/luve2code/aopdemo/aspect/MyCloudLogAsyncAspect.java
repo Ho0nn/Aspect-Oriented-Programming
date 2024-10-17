d97f4af46d709cbd5f33820f2fe35f7958bde891
HoOnn <148750499+Ho0nn@users.noreply.github.com>
2024-10-17 05:11:55 +0300

    AOP Order Pointcuts

--- AOP-Demo-Pointcut-declarations/src/main/java/com/luve2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
+++ AOP-Demo-Pointcut-declarations/src/main/java/com/luve2code/aopdemo/aspect/MyCloudLogAsyncAspect.java
@@ -1,27 +1,19 @@
-
-
 package com.luve2code.aopdemo.aspect;
 
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
-import org.aspectj.lang.annotation.Pointcut;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
-@Order(2)
-public class DemoLoggingAspect {
-
+@Order(1)
+public class MyCloudLogAsyncAspect {
 
 
     @Before("com.luve2code.aopdemo.aspect.AOPExpressions.forDaoPackageNoGetterSetter()")
-    public void beforeAddAccountAdvice() {
-        System.out.println("\n======>>> Executing @Before advice on addAccount()");
+    public void  logToCloudAsync() {
+        System.out.println("\n======>>> Logging to Cloud Async ");
     }
 
-
-
-
 }
-
